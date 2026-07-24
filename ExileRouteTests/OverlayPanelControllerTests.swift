411cd8b8267b662f81ab76acc06329db5c3c3bcd
Swann Macé <swann.mace@abtasty.com>
2026-07-24 11:08:16 +0200

    test(overlay): cover top-left panel placement

---
+++ ExileRouteTests/OverlayPanelControllerTests.swift
@@ -0,0 +1,51 @@
+import AppKit
+import XCTest
+@testable import ExileRoute
+
+final class OverlayPanelControllerTests: XCTestCase {
+    func testDefaultOriginUsesTopLeadingInsets() {
+        let visibleFrame = CGRect(x: 0, y: 25, width: 1_440, height: 875)
+        let panelSize = CGSize(width: 390, height: 230)
+
+        let origin = OverlayPlacement.defaultOrigin(visibleFrame: visibleFrame, panelSize: panelSize)
+
+        XCTAssertEqual(origin.x, 24)
+        XCTAssertEqual(origin.y, 598)
+    }
+
+    func testDefaultOriginSupportsASecondaryDisplayLeftOfMain() {
+        let visibleFrame = CGRect(x: -1_920, y: 0, width: 1_920, height: 1_080)
+        let panelSize = CGSize(width: 390, height: 230)
+
+        let origin = OverlayPlacement.defaultOrigin(visibleFrame: visibleFrame, panelSize: panelSize)
+
+        XCTAssertEqual(origin.x, -1_896)
+        XCTAssertEqual(origin.y, 778)
+    }
+
+    func testResizeKeepsTopLeadingCornerStable() {
+        let currentFrame = CGRect(x: 24, y: 598, width: 390, height: 230)
+
+        let origin = OverlayPlacement.resizedOrigin(
+            currentFrame: currentFrame,
+            newSize: CGSize(width: 460, height: 560)
+        )
+
+        XCTAssertEqual(origin.x, 24)
+        XCTAssertEqual(origin.y, 268)
+    }
+
+    func testOriginIsConstrainedInsideVisibleFrame() {
+        let visibleFrame = CGRect(x: 0, y: 25, width: 1_440, height: 875)
+        let panelSize = CGSize(width: 390, height: 230)
+
+        let origin = OverlayPlacement.constrainedOrigin(
+            CGPoint(x: -100, y: 900),
+            panelSize: panelSize,
+            visibleFrame: visibleFrame
+        )
+
+        XCTAssertEqual(origin.x, 0)
+        XCTAssertEqual(origin.y, 670)
+    }
+}
